99ed58d0fd3171d4d3f071789f126ae0efadedae
Alexus555 <alexus555@yandex.ru>
2020-05-12 22:34:55 +0600

    Created interface IInitDone and class Darial. IInitDone was implemented in class Darial, and method GetComponentName was created.

---
+++ ExtComp1C/AddIn/IInitDone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ExtComp1C.AddIn
+{
+    [Guid("AB634001-F13D-11d0-A459-004095E1DAEA")]// стандартный GUID для IInitDone ссылка http://soaron.fromru.com/vkhints.htm
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInitDone
+    {
+        /// <summary>
+        /// Инициализация компонента
+        /// </summary>
+        /// <param name="connection">reference to IDispatch</param>
+        void Init(
+            [MarshalAs(UnmanagedType.IDispatch)] object connection);
+
+        /// <summary>
+        /// Вызывается перед уничтожением компонента
+        /// </summary>
+        void Done();
+
+        /// <summary>
+        /// Возвращается инициализационная информация
+        /// </summary>
+        /// <param name="info">Component information</param>
+        void GetInfo(
+            [MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref object[] info);
+    }
+}
---
+++ ExtComp1C/Darial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using ExtComp1C.AddIn;
+
+namespace ExtComp1C
+{
+
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Darial : IInitDone
+    {
+        #region "Init"
+
+        /// <summary>
+        /// Инициализация компонента
+        /// </summary>
+        /// <param name="connection">reference to IDispatch</param>
+        public void Init(
+            [MarshalAs(UnmanagedType.IDispatch)] object connection)
+        {
+            //asyncEvent = (IAsyncEvent)connection;
+            //statusLine = (IStatusLine)connection;
+        }
+
+        /// <summary>
+        /// Возвращается информация о компоненте
+        /// </summary>
+        /// <param name="info">Component information</param>
+        public void GetInfo(
+            [MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref object[] info)
+        {
+            info[0] = 2000;
+        }
+
+        public void Done()
+        {
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// Return component's name
+        /// </summary>
+        public string GetComponentName()
+        {
+            return "Darial";
+        }
+
+
+
+        
+    }
+}
